3bb3abc882fd53e7202bcc5cfa2ef18894b8bcbf
Larry Fenske <github@towanda.com>
2014-09-19 17:03:35 -0600

    Set up for packaging.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='scsi_pt',
+    version='0.1.0',
+    license='LICENSE.txt',
+    py_modules=['ScsiPT', 'Cmd', 'CDB', ],
+    scripts=['settime', ],
+    provides=['scsi_pt', 'ScsiPT', 'Cmd', 'CDB', ],
+    install_requires=['listdict', ],
+)
