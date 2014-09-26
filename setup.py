9793025ee477d8fb4cea429cd1dd2fa47b6b9e3d
Larry Fenske <github@towanda.com>
2014-09-26 12:03:45 -0600

    Add more info to setup.py.

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from distutils.core import setup
 setup(
     name='scsi_pt',
     version='0.1.1',
+    description='Python SCSI Toolkit',
+    long_description='a set of classes to create and send SCSI CDBs through libsgutils using templates to create CDBs and data out and to interpret data in',
     license='LICENSE.txt',
     py_modules=['ScsiPT', 'Cmd', 'CDB', ],
+    author="Larry Fenske",
+    author_email="pypi@towanda.com",
+    url="https://github.com/LFenske/python-scsi-pt",
     scripts=['settime', ],
     provides=['scsi_pt', 'ScsiPT', 'Cmd', 'CDB', ],
     install_requires=['listdict', ],
